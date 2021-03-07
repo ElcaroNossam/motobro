abe9c530d95433354322ec2b5ce3acd50c548a91
ElcaroNosam <agent_shops@i.ua>
2021-03-07 17:09:19 +0200

    ops

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("jquery")
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -13,4 +18,4 @@ Turbolinks.start()
 ActiveStorage.start()
 
 
-
+import "bootstrap"
